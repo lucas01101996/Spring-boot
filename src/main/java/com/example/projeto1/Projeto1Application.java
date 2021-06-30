707296f05e2bfb59ed3d4686cf306f0f19110a7d
lucas01101996 <lucas_nogueira155@hotmail.com>
2021-06-30 00:16:53 -0300

    adicionando projeto

---
+++ src/main/java/com/example/projeto1/Projeto1Application.java
@@ -0,0 +1,13 @@
+package com.example.projeto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication//classe principal que vai ficar na raiz do pacote
+public class Projeto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto1Application.class, args);
+	}
+
+}
